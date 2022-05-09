2574ee14959dd14fa5de5c0eb4697e2a60e4043b
fpoku <fpoku92@outlook.com>
2022-05-09 13:12:49 +0100

    newish

--- tasktwoterraform/main.tf
+++ tasktwoterraform/main.tf
@@ -16,11 +16,6 @@ provider "azurerm" {
     #version = "~> 2.46.0"
     features {}
 
-    subscription_id   = "c02ed9ce-f7db-4c51-9614-f1701aa93955"
-    tenant_id         = "1e355c04-e0a4-42ed-8e2d-7351591f0ef1"
-    client_id         = "29eaaa86-8785-4704-8758-063efd1124bd"
-    client_secret     = var.client_secret
-
   }  
 
 module "tasktwo" {
